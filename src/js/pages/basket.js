59635e62d4149c057cfba44b7620846f570b2055
Edward <83923244+TesvintsevDev@users.noreply.github.com>
2023-11-14 02:56:12 +0300

    Update basket.js

--- src/js/pages/basket.js
+++ src/js/pages/basket.js
@@ -7,7 +7,7 @@ export function getBasketPage() {
   const page = document.createElement("div")
   page.classList.add("page", "basket-page")
 
-  const mainTitle = getMainTitle("Козина")
+  const mainTitle = getMainTitle("Корзина")
   const desc = getDesc("Страница в разработке")
 
   // Ссылка оформления заказа
